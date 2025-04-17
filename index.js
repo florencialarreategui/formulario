f2c07589848c582c314a6c54a7fa94233554e69b
florencialarreategui <florencialarreategui@gmail.com>
2025-04-16 23:04:21 -0300

    modifico js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ document.getElementById('pedidoForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const nombre = document.getElementById('nombre').value;
-    const comida = document.getElementById('libreria').value;
-    const bebida = document.querySelector('input[name="tamaño"]:checked').value;
+    const articulo = document.getElementById('productos').value;
+    const tamaño = document.querySelector('input[name="tamaño"]:checked').value;
     const extras = Array.from(document.querySelectorAll('input[name="anillado"]:checked')).map(cb => cb.value);
     const comentarios = document.getElementById('comentarios').value;
 
-    let mensaje = `¡Gracias, ${nombre}! Has pedido ${comida} con ${bebida}.`;
+    let mensaje = `¡Gracias, ${nombre}! Has pedido una ${articulo} tamaño ${tamaño}.`;
     if (extras.length > 0) {
         mensaje += ` Extras: ${extras.join(', ')}.`;
     }
