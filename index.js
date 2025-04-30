a27362312f4b2a4a2af9667c60c53acd47c3018a
florencialarreategui <florencialarreategui@gmail.com>
2025-04-30 00:50:42 -0300

    agrego js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const img = document.querySelector('.silueta');
 document.getElementById('pedidoForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -17,3 +18,22 @@ document.getElementById('pedidoForm').addEventListener('submit', function (e) {
 
     document.getElementById('resultado').textContent = mensaje;
 });
+
+const randomColor = (opacity) => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
+function updateShadow() {
+    const color1 = randomColor(0.6);
+    const color2 = randomColor(0.4);
+    const color3 = randomColor(0.2);
+
+    img.style.filter = `
+    drop-shadow(0 -10px 10px ${color1})
+    drop-shadow(0 -15px 20px ${color2})
+    drop-shadow(0 -25px 40px ${color3})
+  `;
+}
